b239f8c804f5702a990bcdab2cb7204cf3d0748b
backslash112 <yangcun@live.com>
2018-04-22 14:44:47 -0500

    clean code

--- src/main/java/AverageComputation.java
+++ src/main/java/AverageComputation.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -15,20 +12,13 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class AverageComputation {
 
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-        public static final Log log = LogFactory.getLog(Map.class);
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
-            log.info("line: " + line);
             String[] result = line.split(" ");
-            for (String str: result) {
-                log.info(str);
-            }
             String address = result[0];
             String sizeStr = result[result.length - 1];
-            log.info("sizeStr: " + sizeStr);
             int size = sizeStr.equals("-") ? 0 : Integer.parseInt(sizeStr);
-            log.info(String.format("%s: %d(%s)", address, size, sizeStr));
             context.write(new Text(address), new IntWritable(size));
         }
     }
